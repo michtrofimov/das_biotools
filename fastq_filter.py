412438fc0611728e631c65f6d9a2a3d3843b3a49
Michil Trofimov <trofimov.michil@gmail.com>
2023-10-04 21:45:22 +0300

    Add function is_acceptable_quality_score

--- fastq_filter.py
+++ fastq_filter.py
@@ -58,3 +58,72 @@ def is_acceptable_length(seq: str, length_bounds: tuple) -> bool:
     res = lower_threshold <= read_len <= upper_threshold
 
     return res
+
+
+encoding_dict = {
+    "!": 0,
+    '"': 1,
+    "#": 2,
+    "$": 3,
+    "%": 4,
+    "&": 5,
+    "'": 6,
+    "(": 7,
+    ")": 8,
+    "*": 9,
+    "+": 10,
+    ",": 11,
+    "-": 12,
+    ".": 13,
+    "/": 14,
+    "0": 15,
+    "1": 16,
+    "2": 17,
+    "3": 18,
+    "4": 19,
+    "5": 20,
+    "6": 21,
+    "7": 22,
+    "8": 23,
+    "9": 24,
+    ":": 25,
+    ";": 26,
+    "<": 27,
+    "=": 28,
+    ">": 29,
+    "?": 30,
+    "@": 31,
+    "A": 32,
+    "B": 33,
+    "C": 34,
+    "D": 35,
+    "E": 36,
+    "F": 37,
+    "G": 38,
+    "H": 39,
+    "I": 40,
+}
+
+
+def is_acceptable_quality_score(seq: str, quality_threshold: int) -> bool:
+    """
+    Check if the average quality score of a sequence is above a specified threshold.
+
+    Parameters:
+    - seq (str): sequence of quality scores
+    - quality_threshold (int): the minimum acceptable quality score
+
+    Returns:
+    - bool: True if the average quality score is above the threshold, False otherwise
+    """
+
+    read_quality_score = 0
+
+    for score in seq:
+        read_quality_score += encoding_dict[score]
+
+    read_quality_score = read_quality_score / len(seq)
+
+    res = read_quality_score >= quality_threshold
+
+    return res
