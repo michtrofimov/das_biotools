252035e52157b368d0d5395f090d5f16c36280af
Michil Trofimov <trofimov.michil@gmail.com>
2023-10-04 21:44:23 +0300

    Add function is_acceptable_gc

--- fastq_filter.py
+++ fastq_filter.py
@@ -18,3 +18,23 @@ def calculate_gc_content(seq: str) -> float:
     gc_content = gc_content / len(seq) * 100
 
     return gc_content
+
+
+def is_acceptable_gc(seq: str, gc_bounds: tuple) -> bool:
+    """
+    Check if the GC content of a DNA sequence within specified bounds
+
+    Parameters:
+    - seq (str): DNA sequence
+    - gc_bounds (tuple): tuple with lower and upper bounds for GC content
+
+    Returns:
+    - bool: True if GC content is within bounds, False otherwise
+    """
+
+    gc_content = calculate_gc_content(seq)
+    lower_threshold, upper_threshold = gc_bounds
+
+    res = lower_threshold <= gc_content <= upper_threshold
+
+    return res
