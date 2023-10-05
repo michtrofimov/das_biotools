a7dda3f10e61177957e9ae42b0317119b7f33fa2
Michil Trofimov <trofimov.michil@gmail.com>
2023-10-05 22:41:47 +0300

    Add docstrings to dna_rna_tools

--- src/das_dna_rna_tools.py
+++ src/das_dna_rna_tools.py
@@ -1,4 +1,13 @@
-def check_if_procedure(procedure):
+def check_if_procedure(procedure: str) -> None:
+    """
+    Check if the specified procedure is valid
+
+    Parameters:
+    - procedure (str): DNA/RNA processing procedure
+
+    Raises:
+    - ValueError: if the procedure is not in the supported list
+    """
     procedures = ["transcribe", "reverse", "complement", "reverse_complement"]
 
     if procedure not in procedures:
@@ -7,13 +16,37 @@ def check_if_procedure(procedure):
         )
 
 
-def check_if_empty(seqs):
+def check_if_empty(seqs: list) -> None:
+    """
+    Check if the input sequence list is empty
+
+    Parameters:
+    - seqs (list): DNA/RNA sequences
+
+    Raises:
+    - ValueError: if the input sequence list is empty
+    """
+
     # check if input is empty
     if len(seqs) == 0:
         raise ValueError("No sequences in input!")
 
 
-def filter_seqs(seqs, dna_alphabet="ATGC", rna_alphabet="AUGC"):
+def filter_seqs(
+    seqs: list, dna_alphabet: str = "ATGC", rna_alphabet: str = "AUGC"
+) -> list:
+    """
+    Filter out non-nucleotide sequences from the input list
+
+    Parameters:
+    - seqs (list): DNA/RNA sequences
+    - dna_alphabet (str): DNA alphabet characters
+    - rna_alphabet (str): RNA alphabet characters
+
+    Returns:
+    - list: filtered list of DNA/RNA sequences
+    """
+
     check_if_empty(seqs)
     for seq in seqs:
         # check if sequence is not a nucleotide sequence and pop it
@@ -29,7 +62,17 @@ def filter_seqs(seqs, dna_alphabet="ATGC", rna_alphabet="AUGC"):
     return seqs
 
 
-def transcribe(seq):
+def transcribe(seq: str) -> str:
+    """
+    transcribe a DNA sequence to an RNA sequence
+
+    Parameters:
+    - seq (str): DNA sequence
+
+    Returns:
+    - str: transcribed RNA sequence
+    """
+
     transcribed = ""
 
     for nt in seq:
@@ -48,7 +91,17 @@ def transcribe(seq):
     return transcribed
 
 
-def reverse_seq(seq):
+def reverse_seq(seq: str) -> str:
+    """
+    Reverse a DNA/RNA sequence
+
+    Parameters:
+    - seq (str): input DNA/RNA sequence
+
+    Returns:
+    - str: reversed sequence
+    """
+
     return seq[::-1]
 
 
@@ -74,21 +127,51 @@ COMPLEMENTATION_RNA = {
 }
 
 
-def is_dna(seq):
+def is_dna(seq: str) -> bool:
+    """
+    Check if a sequence is DNA
+
+    Parameters:
+    - seq (str): input sequence
+
+    Returns:
+    - bool: True if the sequence is DNA, False otherwise
+    """
+
     if ("T" in seq) or ("t" in seq):
         return True
     else:
         return False
 
 
-def is_rna(seq):
+def is_rna(seq: str) -> bool:
+    """
+    Check if a sequence is RNA
+
+    Parameters:
+    - seq (str): Input sequence
+
+    Returns:
+    - bool: True if the sequence is RNA, False otherwise
+    """
+
     if ("U" in seq) or ("u" in seq):
         return True
     else:
         return False
 
 
-def complement(seq):
+def complement(seq: str) -> str:
+    """
+    Generate the complement of a DNA/RNA sequence
+
+    Parameters:
+    - seq (str): Input sequence.
+
+    Returns:
+    - str: Complemented sequence.
+    """
+
     if (not is_dna(seq)) and (not is_rna(seq)):
         print("Ambigious sequence. Considering it as DNA")
         res = "".join([COMPLEMENTATION_DNA[nt] for nt in seq])
@@ -102,13 +185,42 @@ def complement(seq):
     return res
 
 
-def reverse_complement(seq):
+def reverse_complement(seq: str) -> str:
+    """
+    Generate the reverse complement of a DNA/RNA sequence
+
+    Parameters:
+    - seq (str): Input sequence
+
+    Returns:
+    - str: Reverse complemented sequence
+    """
+
     res = complement(reverse_seq(seq))
 
     return res
 
 
-def dna_rna_tools(*seqs):
+def dna_rna_tools(*seqs: str) -> str:
+    """
+    Perform DNA/RNA operations based on the specified procedure
+
+    Parameters:
+    - *seqs (str): Variable number of DNA/RNA sequences, last element is a DNA/RNA processing procedure
+
+    Returns:
+    - str: Result of the specified procedure on the sequences.
+
+    Supported Procedures:
+    - "transcribe": Transcribe DNA sequences to RNA
+    - "reverse": Reverse the sequences
+    - "complement": Generate the complement of DNA/RNA sequences
+    - "reverse_complement": Generate the reverse complement of DNA/RNA sequences
+
+    Raises:
+    - ValueError: If the specified procedure is not supported.
+    """
+
     # parse seqs list
     *seqs, procedure = seqs
 
